6614099e8d3c7767d9d9c4305a321c14394ff7ea
rameessiyad <rameesmsiyadofficial786@gmail.com>
2024-11-09 17:05:48 +0530

    feat: added animated mouse cursor

---
+++ client/src/components/cursor/Cursor.jsx
@@ -0,0 +1,27 @@
+import { useEffect, useState } from "react";
+import { motion } from "framer-motion";
+import "./cursor.scss";
+
+const Cursor = () => {
+  const [position, setPosition] = useState({ x: 0, y: 0 });
+
+  useEffect(() => {
+    const handleMouseMove = (e) => {
+      setPosition({ x: e.clientX, y: e.clientY });
+    };
+
+    window.addEventListener("mousemove", handleMouseMove);
+
+    return () => {
+      window.removeEventListener("mousemove", handleMouseMove);
+    };
+  }, []);
+  return (
+    <motion.div
+      className="cursor"
+      animate={{ x: position.x, y: position.y }}
+    ></motion.div>
+  );
+};
+
+export default Cursor;
